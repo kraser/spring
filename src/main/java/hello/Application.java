17a6ed677716c69e37a91799013297729a17c364
kraser <kravalsergey@yandex.ru>
2018-09-02 21:42:17 +0700

    gs-uploading-files ru

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,52 +1,28 @@
 package hello;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
+import javax.servlet.MultipartConfigElement;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class Application {
 
-    public static void main(String args[]) {
-        // простой DS для тестирования (не для реального использования!)
-        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-        dataSource.setDriverClass(org.h2.Driver.class);
-        dataSource.setUsername("sa");
-        dataSource.setUrl("jdbc:h2:mem");
-        dataSource.setPassword("");
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("128KB");
+        factory.setMaxRequestSize("128KB");
+        return factory.createMultipartConfig();
+    }
 
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-
-        System.out.println("Creating tables");
-        jdbcTemplate.execute("drop table customers if exists");
-        jdbcTemplate.execute("create table customers(" +
-                "id serial, first_name varchar(255), last_name varchar(255))");
-
-        String[] names = "John Woo;Jeff Dean;Josh Bloch;Josh Long".split(";");
-        for (String fullname : names) {
-            String[] name = fullname.split(" ");
-            System.out.printf("Inserting customer record for %s %s\n", name[0], name[1]);
-            jdbcTemplate.update(
-                    "INSERT INTO customers(first_name,last_name) values(?,?)",
-                    name[0], name[1]);
-        }
-
-        System.out.println("Querying for customer records where first_name = 'Josh':");
-        List<Customer> results = jdbcTemplate.query(
-                "select * from customers where first_name = ?", new Object[] { "Josh" },
-                new RowMapper<Customer>() {
-                    @Override
-                    public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        return new Customer(rs.getLong("id"), rs.getString("first_name"),
-                                rs.getString("last_name"));
-                    }
-                });
-
-        for (Customer customer : results) {
-            System.out.println(customer);
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
